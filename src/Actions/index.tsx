246fc89e89cea6720500829666b755d18683466c
Hannah Nordgren <hey@hereshannahs.info>
2020-03-12 00:05:41 -0400

    [MISC] added actions

--- src/Actions/index.tsx
+++ src/Actions/index.tsx
@@ -1,1 +1,20 @@
+import { todosRef } from '../firebase.ts';
 export const FETCH_TODOS = 'FETCH_TODOS';
+
+export const addTodo = (newTodo: any) => async dispatch => {
+	todosRef.push().set(newTodo);
+};
+
+export const completeTodo = (completeTodo: any) => async dispatch => {
+	todosRef.child(completeTodo).remove();
+};
+
+export const fetchTodos = () => async dispatch => {
+	todosRef.on('value', snapshot => {
+		dispatch({
+			type: FETCH_TODOS,
+			payload: snapshot.val()
+		});
+	});
+};
+
